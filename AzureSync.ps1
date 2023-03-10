aa1ec681079329f7e5fde823e81717619100fdf4
jframptondev <joseph@frampton.dev>
2023-03-10 10:43:18 +0000

    Create AzureSync.ps1
    Takes two arguments (the top two folder levels) and using azure identity management within the VM copies all of the files in that directory to a new blob storage container.

---
+++ AzureSync.ps1
@@ -0,0 +1,17 @@
+﻿azcopy login --identity
+$1 = $args[0]
+$2 = $args[1]
+$accounturl = "https://account.blob.core.windows.net"
+$accountname = "accountname"
+$accountkey = ""
+$azureStorageContainer = "myquestablobs"
+$azureContext = New-AzureStorageContext -ConnectionString "DefaultEndpointsProtocol=https;AccountName=$accountname;AccountKey=$accountkey;EndpointSuffix=core.windows.net;"
+$blobchk = Get-AzureStorageBlob -Blob "$accounturl/$1$2" -Container $azureStorageContainer -Context $azureContext -ErrorAction Ignore
+if (-not $blobchk)
+{
+    $NewBlobContainer = "$accounturl/$1$2".ToLower()
+    azcopy make "$NewBlobContainer"
+}
+$NewBlobContainer = "$accounturl/$1$2".ToLower()
+azcopy make "$NewBlobContainer"
+azcopy sync "$PSScriptRoot\$1\$2" "$NewBlobContainer" --delete-destination=true
